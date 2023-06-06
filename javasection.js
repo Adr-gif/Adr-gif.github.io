0d6fb6e325d7e50242dd63b23d898156fd7983d2
Adr-gif <128637241+Adr-gif@users.noreply.github.com>
2023-06-06 13:24:10 -0300

    agregue los datos enviados al DOM

--- javasection.js
+++ javasection.js
@@ -1,50 +1,50 @@
 function carga() {
-  
-    var name = document.getElementById("name").value;
-    var surname = document.getElementById("surname").value;
-    var tel = document.getElementById("tel").value;
-    var email = document.getElementById("email").value;
-    var areaTexto = document.getElementById("areaTexto").value;
-    var errorMensaje = document.getElementById("errorMensaje");
-  
-    if (name === "" || surname === "" || tel === "" || email === "" || areaTexto === "") {
-      
-      var errorMensaje = document.getElementById("datosEnviados");
-      errorMensaje.innerHTML = "Por favor, complete todos los campos obligatorios.";
-      errorMensaje.style.color = "red";
-      return false;
-    }
-  
-    // Validar la longitud máxima
-    if (name.length > 50 || surname.length > 50 || tel.length > 20 || email.length > 50 || areaTexto.length > 500) {
-      // Mostrar mensaje de error si algún campo excede la longitud máxima
-      var errorMensaje = document.getElementById("datosEnviados");
-      errorMensaje.innerHTML = "Algunos campos exceden la longitud máxima permitida.";
-      errorMensaje.style.color = "red";
-      return false;
-    }
-  
-    // Validar el formato del email utilizando una expresión regular
-    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!emailRegex.test(email)) {
-      // Mostrar mensaje de error si el formato del email es incorrecto
-      var errorMensaje = document.getElementById("datosEnviados");
-      errorMensaje.innerHTML = "Por favor, ingrese un email válido.";
-      errorMensaje.style.color = "red";
-      return false;
-    }
-    var mensaje = "Datos enviados:\n\n" +
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var tel = document.getElementById("tel").value;
+  var email = document.getElementById("email").value;
+  var areaTexto = document.getElementById("areaTexto").value;
+  var errorMensaje = document.getElementById("errorMensaje");
+
+  if (name === "" || surname === "" || tel === "" || email === "" || areaTexto === "") {
+    var errorMensaje = document.getElementById("datosEnviados");
+    errorMensaje.innerHTML = "Por favor, complete todos los campos obligatorios.";
+    errorMensaje.style.color = "red";
+    return false;
+  }
+
+  // Validar la longitud máxima
+  if (name.length > 50 || surname.length > 50 || tel.length > 20 || email.length > 50 || areaTexto.length > 500) {
+    var errorMensaje = document.getElementById("datosEnviados");
+    errorMensaje.innerHTML = "Algunos campos exceden la longitud máxima permitida.";
+    errorMensaje.style.color = "red";
+    return false;
+  }
+
+  // Validar el formato del email utilizando una expresión regular
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!emailRegex.test(email)) {
+    var errorMensaje = document.getElementById("datosEnviados");
+    errorMensaje.innerHTML = "Por favor, ingrese un email válido.";
+    errorMensaje.style.color = "red";
+    return false;
+  }
+
+  var mensaje = "Datos enviados:\n\n" +
                 "Nombre: " + name + "\n" +
                 "Apellido: " + surname + "\n" +
                 "Teléfono: " + tel + "\n" +
                 "Email: " + email + "\n" +
                 "Comentarios: " + areaTexto;
 
+  var datosEnviadosDiv = document.getElementById("datosEnviados");
+  datosEnviadosDiv.innerHTML = mensaje;
+
   alert(mensaje);
   alert("Gracias por completar el formulario");
-    return false;
-  }
-  function reseteando(){
-    document.getElementById("campos").reset();
-    
+  return false;
+}
+
+function reseteando(){
+  document.getElementById("campos").reset();
 }
